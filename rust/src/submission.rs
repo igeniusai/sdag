872dc31660221d66e38ea3142b5da87745c311e7
Alessandro Rognoni <alessandro.rognoni@domyn.com>
2025-10-30 14:02:38 +0100

    add submission tests

--- rust/src/submission.rs
+++ rust/src/submission.rs
@@ -6,6 +6,7 @@ use serde_json;
 use std::collections::HashMap;
 use std::error::Error;
 use std::io;
+
 #[derive(Debug, Clone)]
 pub struct Submitter<'a, T: Backend, U: StateManager> {
     pub backend: &'a T,
@@ -93,168 +94,319 @@ impl<'a, T: Backend, U: StateManager> Submitter<'a, T, U> {
     }
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use crate::model::Parent;
-//
-//    use super::*;
-//    use std::path::PathBuf;
-//
-//    struct MockBackend;
-//    impl Backend for MockBackend {
-//        fn update_status(&self, _nodemap: &mut HashMap<String, Node>) {}
-//        fn submit(&self, _launch_script: &str, _uid: &str, _pipeline_dir: &str) -> String {
-//            String::from("submitted")
-//        }
-//    }
-//
-//    struct MockState {
-//        fake_path: PathBuf,
-//    }
-//    impl MockState {
-//        fn new() -> Self {
-//            Self {
-//                fake_path: PathBuf::from("path"),
-//            }
-//        }
-//    }
-//
-//    impl StateManager for MockState {
-//        fn prepare(&self, _dag: &crate::model::DAG) {}
-//        fn copy_output(&self, _src_uid: &str, _dst_uid: &str) -> std::io::Result<u64> {
-//            std::io::Result::Ok(1)
-//        }
-//        fn read_output(&self, _uid: &str) -> std::io::Result<String> {
-//            std::io::Result::Ok(String::from(r#"{"output":true}"#))
-//        }
-//        fn get_pipeline_dir(&self) -> &PathBuf {
-//            &self.fake_path
-//        }
-//    }
-//
-//    fn get_submitter() -> Submitter<MockBackend, MockState> {
-//        Submitter {
-//            backend: MockBackend,
-//            state: MockState::new(),
-//        }
-//    }
-//
-//    #[test]
-//    fn find_parent_uid_for_oneof() {
-//        let submitter = get_submitter();
-//
-//        let parent_statuses = HashMap::from([
-//            (String::from("a"), JobStatus::NotSubmitted),
-//            (String::from("b"), JobStatus::Completed),
-//            (String::from("c"), JobStatus::Failed),
-//        ]);
-//
-//        let uid = submitter.find_completed_parent_uid(&parent_statuses);
-//        assert_eq!(uid, String::from("b"));
-//    }
-//
-//    #[test]
-//    fn submit_root() {
-//        let mut node = Node {
-//            uid: String::from("c"),
-//            behavior: NodeBehavior::RootNode {
-//                children: Vec::new(),
-//            },
-//            status: JobStatus::ReadyForSubmission,
-//            parents: vec![Parent {
-//                name: String::from("p"),
-//                uid: String::from("p"),
-//            }],
-//        };
-//
-//        let parent_statuses = HashMap::from([(String::from("c"), JobStatus::Completed)]);
-//        let submitter = get_submitter();
-//        submitter.submit_based_on_behavior(&mut node, &parent_statuses);
-//        assert!(matches!(node.status, JobStatus::Completed))
-//    }
-//
-//    #[test]
-//    fn submit_end() {
-//        let mut node = Node {
-//            uid: String::from("c"),
-//            behavior: NodeBehavior::EndNode {
-//                children: Vec::new(),
-//            },
-//            status: JobStatus::ReadyForSubmission,
-//            parents: vec![Parent {
-//                name: String::from("p"),
-//                uid: String::from("p"),
-//            }],
-//        };
-//
-//        let parent_statuses = HashMap::from([(String::from("c"), JobStatus::Completed)]);
-//        let submitter = get_submitter();
-//        submitter.submit_based_on_behavior(&mut node, &parent_statuses);
-//        assert!(matches!(node.status, JobStatus::Completed))
-//    }
-//
-//    #[test]
-//    fn submit_if() {
-//        let mut node = Node {
-//            uid: String::from("c"),
-//            behavior: NodeBehavior::IfNode {
-//                selected: None,
-//                true_branch: Vec::new(),
-//                false_branch: Vec::new(),
-//            },
-//            status: JobStatus::ReadyForSubmission,
-//            parents: vec![Parent {
-//                name: String::from("p"),
-//                uid: String::from("p"),
-//            }],
-//        };
-//
-//        let parent_statuses = HashMap::from([(String::from("c"), JobStatus::Completed)]);
-//        let submitter = get_submitter();
-//        submitter.submit_based_on_behavior(&mut node, &parent_statuses);
-//        assert!(matches!(node.status, JobStatus::Completed));
-//    }
-//
-//    #[test]
-//    fn submit_task() {
-//        let mut node = Node {
-//            uid: String::from("c"),
-//            behavior: NodeBehavior::TaskNode {
-//                fname: String::from("fname"),
-//                launch_script: String::from("submit.sh"),
-//                return_type: None,
-//                children: Vec::new(),
-//            },
-//            status: JobStatus::ReadyForSubmission,
-//            parents: vec![Parent {
-//                name: String::from("p"),
-//                uid: String::from("p"),
-//            }],
-//        };
-//
-//        let parent_statuses = HashMap::from([(String::from("c"), JobStatus::Completed)]);
-//        let submitter = get_submitter();
-//        submitter.submit_based_on_behavior(&mut node, &parent_statuses);
-//        assert!(matches!(node.status, JobStatus::Running(_)));
-//    }
-//
-//    #[test]
-//    fn submit_oneof() {
-//        let mut node = Node {
-//            uid: String::from("c"),
-//            behavior: NodeBehavior::OneOfNode {
-//                children: Vec::new(),
-//            },
-//            status: JobStatus::ReadyForSubmission,
-//            parents: vec![Parent {
-//                name: String::from("p"),
-//                uid: String::from("p"),
-//            }],
-//        };
-//
-//        let parent_statuses = HashMap::from([(String::from("c"), JobStatus::Completed)]);
-//        let submitter = get_submitter();
-//        submitter.submit_based_on_behavior(&mut node, &parent_statuses);
-//        assert!(matches!(node.status, JobStatus::Completed));
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use crate::model::{DAG, Parent};
+
+    use super::*;
+    use std::path::PathBuf;
+
+    struct MockBackend;
+    impl Backend for MockBackend {
+        fn update_status(&self, _nodemap: &mut HashMap<String, Node>) {}
+        fn submit(
+            &self,
+            _launch_script: &str,
+            _uid: &str,
+            _pipeline_dir: &PathBuf,
+        ) -> Result<String, Box<dyn Error>> {
+            Ok(String::from("1234"))
+        }
+    }
+
+    struct MockState {
+        fake_path: PathBuf,
+    }
+    impl MockState {
+        fn new() -> Self {
+            Self {
+                fake_path: PathBuf::from("path"),
+            }
+        }
+    }
+
+    impl StateManager for MockState {
+        fn prepare(&self, _dag: &DAG) -> io::Result<()> {
+            Ok(())
+        }
+        fn copy_output(&self, _src_uid: &str, _dst_uid: &str) -> std::io::Result<u64> {
+            std::io::Result::Ok(1)
+        }
+        fn read_output(&self, _uid: &str) -> std::io::Result<String> {
+            std::io::Result::Ok(String::from(r#"{"output":true}"#))
+        }
+        fn get_pipeline_dir(&self) -> &PathBuf {
+            &self.fake_path
+        }
+        fn copy_dag_into_working_dir(&self, _path: &PathBuf) -> io::Result<u64> {
+            Ok(1)
+        }
+    }
+
+    #[test]
+    fn find_parent_uid_for_oneof() {
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+        let parent_statuses = HashMap::from([
+            (String::from("a"), JobStatus::NotSubmitted),
+            (String::from("b"), JobStatus::Completed(NodeResult::Node)),
+            (String::from("c"), JobStatus::Failed),
+        ]);
+
+        let uid = submitter.find_completed_parent_uid(&parent_statuses);
+        assert_eq!(uid, String::from("b"));
+    }
+
+    #[test]
+    fn submit_root() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::RootNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: vec![Parent {
+                name: String::from("p"),
+                uid: String::from("p"),
+            }],
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([("c".to_string(), node.clone())]);
+        let new_status = submitter.submit_node(&node, &nodemap);
+        assert!(matches!(new_status, JobStatus::Completed(NodeResult::Node)))
+    }
+
+    #[test]
+    fn submit_end() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::EndNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: vec![Parent {
+                name: String::from("p"),
+                uid: String::from("p"),
+            }],
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([("c".to_string(), node)]);
+        let node = nodemap.get("c").unwrap();
+        let new_status = submitter.submit_node(node, &nodemap);
+        assert!(matches!(new_status, JobStatus::Completed(NodeResult::Node)))
+    }
+
+    #[test]
+    fn submit_if() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::IfNode {
+                true_branch: Vec::new(),
+                false_branch: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: vec![Parent {
+                name: String::from("p"),
+                uid: String::from("p"),
+            }],
+        };
+
+        let parent = Node {
+            uid: String::from("p"),
+            behavior: NodeBehavior::RootNode {
+                children: vec!["c".to_string()],
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([("c".to_string(), node), ("p".to_string(), parent)]);
+        let node = nodemap.get("c").unwrap();
+        let new_status = submitter.submit_node(node, &nodemap);
+        assert!(matches!(
+            new_status,
+            JobStatus::Completed(NodeResult::If(true))
+        ))
+    }
+
+    #[test]
+    fn submit_task() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::TaskNode {
+                fname: String::from("function"),
+                launch_script: String::from("script"),
+                return_type: None,
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: vec![Parent {
+                name: String::from("p"),
+                uid: String::from("p"),
+            }],
+        };
+
+        let parent = Node {
+            uid: String::from("p"),
+            behavior: NodeBehavior::RootNode {
+                children: vec!["c".to_string()],
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([("c".to_string(), node), ("p".to_string(), parent)]);
+        let node = nodemap.get("c").unwrap();
+        let new_status = submitter.submit_node(node, &nodemap);
+        assert!(matches!(new_status, JobStatus::Running(_)))
+    }
+
+    #[test]
+    fn submit_oneof() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::OneOfNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: vec![
+                Parent {
+                    name: String::from("p1"),
+                    uid: String::from("p1"),
+                },
+                Parent {
+                    name: String::from("p2"),
+                    uid: String::from("p2"),
+                },
+            ],
+        };
+
+        let p1 = Node {
+            uid: String::from("p1"),
+            behavior: NodeBehavior::RootNode {
+                children: vec!["c".to_string()],
+            },
+            status: JobStatus::Failed,
+            parents: Vec::new(),
+        };
+
+        let p2 = Node {
+            uid: String::from("p2"),
+            behavior: NodeBehavior::RootNode {
+                children: vec!["c".to_string()],
+            },
+            status: JobStatus::Completed(NodeResult::Node),
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([
+            ("c".to_string(), node),
+            ("p1".to_string(), p1),
+            ("p2".to_string(), p2),
+        ]);
+        let node = nodemap.get("c").unwrap();
+        let new_status = submitter.submit_node(node, &nodemap);
+        assert!(matches!(new_status, JobStatus::Completed(_)))
+    }
+
+    #[test]
+    fn find_updated_statuses() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::RootNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let nodemap = HashMap::from([("c".to_string(), node)]);
+        let updated_statuses = submitter.find_updated_statuses(&nodemap);
+        assert_eq!(
+            updated_statuses,
+            HashMap::from([("c".to_string(), JobStatus::Completed(NodeResult::Node))])
+        )
+    }
+
+    #[test]
+    fn update_status() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::RootNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let mut nodemap = HashMap::from([("c".to_string(), node)]);
+        let updated_statuses = HashMap::from([("c".to_string(), JobStatus::Failed)]);
+
+        submitter.update_status(&mut nodemap, updated_statuses);
+        let child = nodemap.get("c").unwrap();
+        assert!(matches!(child.status, JobStatus::Failed))
+    }
+
+    #[test]
+    fn e2e() {
+        let node = Node {
+            uid: String::from("c"),
+            behavior: NodeBehavior::RootNode {
+                children: Vec::new(),
+            },
+            status: JobStatus::ReadyForSubmission,
+            parents: Vec::new(),
+        };
+
+        let submitter = Submitter {
+            backend: &MockBackend,
+            state: &MockState::new(),
+        };
+
+        let mut nodemap = HashMap::from([("c".to_string(), node)]);
+
+        submitter.submit(&mut nodemap);
+        let child = nodemap.get("c").unwrap();
+        assert!(matches!(
+            child.status,
+            JobStatus::Completed(NodeResult::Node)
+        ))
+    }
+}
