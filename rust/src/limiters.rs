ed2e0a041e3447489ecde18ed9a4ba7e9ac49a3c
arognoni-domyn <alessandro.rognoni@domyn.com>
2025-12-05 17:34:13 +0100

    look also at running jobs to block cacheable tasks

--- rust/src/limiters.rs
+++ rust/src/limiters.rs
@@ -1,8 +1,15 @@
+//! Limit the number of cacheable running tasks.
+//!
+//! Only one cacheable task of a given name is allowed to be running.
+//! This helps saving resources: One task is executed and then
+//! everything else uses its cached result.
+
 use crate::model::{JobStatus, Node, NodeBehavior};
 use std::collections::{HashMap, HashSet};
 
+/// Limit the number of running cacheable tasks
 pub fn limit_cached_tasks_same_name(nodemap: &mut HashMap<String, Node>) {
-    let mut submitted_with_caching: HashSet<String> = HashSet::new();
+    let mut submitted_with_caching = find_running_cacheable_jobs(nodemap);
     for node in nodemap.values_mut() {
         if let JobStatus::ReadyForSubmission = node.status
             && let NodeBehavior::TaskNode { caching, fname, .. } = &node.behavior
@@ -17,9 +24,28 @@ pub fn limit_cached_tasks_same_name(nodemap: &mut HashMap<String, Node>) {
     }
 }
 
+/// Find all cacheable running task names
+fn find_running_cacheable_jobs(nodemap: &HashMap<String, Node>) -> HashSet<String> {
+    nodemap
+        .values()
+        .filter(|n| matches!(n.status, JobStatus::Running(_)))
+        .filter_map(|n| {
+            if let NodeBehavior::TaskNode { caching, fname, .. } = &n.behavior
+                && *caching
+            {
+                Some(fname.to_string())
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Two cacheable tasks with the same name, only one survives.
     #[test]
     fn test_limit_task_execution() {
         let mut nodemap = HashMap::from([
@@ -69,5 +95,105 @@ mod tests {
             .filter(|n| matches!(n.status, JobStatus::ReadyForSubmission))
             .count();
         assert_eq!(nready, 1);
+    }
+
+    // One task with the same name is running, nothing is scheduled
+    #[test]
+    fn test_limit_task_execution_because_running() {
+        let mut nodemap = HashMap::from([
+            (
+                String::from("0"),
+                Node {
+                    uid: String::from("0"),
+                    output_artifacts: Vec::new(),
+                    output_used: false,
+                    parents: Vec::new(),
+                    children: Vec::new(),
+                    status: JobStatus::ReadyForSubmission,
+                    behavior: NodeBehavior::TaskNode {
+                        fname: String::from("fname"),
+                        caching: true,
+                        try_num: 0,
+                        retries: 0,
+                        launch_script: String::from("lauch.sh"),
+                        input_kwargs: Vec::new(),
+                    },
+                },
+            ),
+            (
+                String::from("1"),
+                Node {
+                    uid: String::from("1"),
+                    output_artifacts: Vec::new(),
+                    output_used: false,
+                    parents: Vec::new(),
+                    children: Vec::new(),
+                    status: JobStatus::Running("123".to_string()),
+                    behavior: NodeBehavior::TaskNode {
+                        fname: String::from("fname"),
+                        caching: true,
+                        try_num: 0,
+                        retries: 0,
+                        launch_script: String::from("lauch.sh"),
+                        input_kwargs: Vec::new(),
+                    },
+                },
+            ),
+        ]);
+
+        limit_cached_tasks_same_name(&mut nodemap);
+        let nready = nodemap
+            .values()
+            .filter(|n| matches!(n.status, JobStatus::ReadyForSubmission))
+            .count();
+        assert_eq!(nready, 0);
+    }
+
+    // Check only cacheable running task names are detected
+    #[test]
+    fn test_running_cacheable_jobs() {
+        let nodemap = HashMap::from([
+            (
+                String::from("0"),
+                Node {
+                    uid: String::from("0"),
+                    output_artifacts: Vec::new(),
+                    output_used: false,
+                    parents: Vec::new(),
+                    children: Vec::new(),
+                    status: JobStatus::Running("1".to_string()),
+                    behavior: NodeBehavior::TaskNode {
+                        fname: String::from("fname"),
+                        caching: true,
+                        try_num: 0,
+                        retries: 0,
+                        launch_script: String::from("lauch.sh"),
+                        input_kwargs: Vec::new(),
+                    },
+                },
+            ),
+            (
+                String::from("1"),
+                Node {
+                    uid: String::from("1"),
+                    output_artifacts: Vec::new(),
+                    output_used: false,
+                    parents: Vec::new(),
+                    children: Vec::new(),
+                    status: JobStatus::ReadyForSubmission,
+                    behavior: NodeBehavior::TaskNode {
+                        fname: String::from("not-running"),
+                        caching: true,
+                        try_num: 0,
+                        retries: 0,
+                        launch_script: String::from("lauch.sh"),
+                        input_kwargs: Vec::new(),
+                    },
+                },
+            ),
+        ]);
+
+        let set = find_running_cacheable_jobs(&nodemap);
+        assert_eq!(set, HashSet::from(["fname".to_string()]));
     }
 }
